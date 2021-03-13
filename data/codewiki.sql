483753c21c44244010efd82f3d00232edf49d88b
wed7590 <wed7590@naver.com>
2021-03-13 16:47:42 +0900

    sql문 수정

--- data/codewiki.sql
+++ data/codewiki.sql
@@ -1,13 +1,16 @@
+CREATE DATABASE codewiki;
+USE codewiki;
+
 CREATE TABLE `User` (
  `user_id` varchar(15) NOT NULL,
- `visit_wip` varchar(15) NOT NULL,
- `visit_date` varchar(15) NOT NULL,
  `user_password` varchar(15) NOT NULL,
  `user_email` varchar(30) NOT NULL,
  `user_name` varchar(15) NOT NULL,
  `user_join_date` varchar(15) NOT NULL,
  `user_grade` varchar(15) NOT NULL DEFAULT 1,
- `user_token` varchar(15) NULL
+ `user_token` varchar(15) NULL,
+ `visit_wip` varchar(15) NOT NULL,
+ `visit_date` varchar(15) NOT NULL,
 );
 
 CREATE TABLE `Board` (
@@ -77,128 +80,23 @@ CREATE TABLE `codewiki_list` (
  `code_list3_name` varchar(30) NULL
 );
 
---관리자 id 생성
-insert into User values ('admin', '000.000.000.000', '1970-01-01', '123456', 'admin@codewiki.co.kr', 'administrator', '1970-01-01', '5', '');
+ALTER TABLE `codewiki_list` ADD CONSTRAINT `FK_codewiki_TO_codewiki_list_1` FOREIGN KEY (
+ `code_seq`
+)
+REFERENCES `codewiki` (
+ `code_seq`
+);
 
---ALTER TABLE `User` ADD CONSTRAINT `PK_USER` PRIMARY KEY (
--- `user_id`,
--- `visit_wip`,
--- `visit_date`
---);
---
---ALTER TABLE `Board` ADD CONSTRAINT `PK_BOARD` PRIMARY KEY (
--- `board_seq`
---);
---
---ALTER TABLE `Board_comment` ADD CONSTRAINT `PK_BOARD_COMMENT` PRIMARY KEY (
--- `boardcom_seq`
---);
---
---ALTER TABLE `codewiki` ADD CONSTRAINT `PK_CODEWIKI` PRIMARY KEY (
--- `code_seq`
---);
---
---ALTER TABLE `Argument` ADD CONSTRAINT `PK_ARGUMENT` PRIMARY KEY (
--- `argu_seq`
---);
---
---ALTER TABLE `Argument_view` ADD CONSTRAINT `PK_ARGUMENT_VIEW` PRIMARY KEY (
--- `arguview_seq`
---);
---
---ALTER TABLE `codewiki_comment` ADD CONSTRAINT `PK_CODEWIKI_COMMENT` PRIMARY KEY (
--- `codecom_seq`
---);
---
---ALTER TABLE `visitor` ADD CONSTRAINT `PK_VISITOR` PRIMARY KEY (
--- `visit_wip`,
--- `visit_date`
---);
---
---ALTER TABLE `codewiki_list` ADD CONSTRAINT `PK_CODEWIKI_LIST` PRIMARY KEY (
--- `code_seq`
---);
---
---ALTER TABLE `User` ADD CONSTRAINT `FK_visitor_TO_User_1` FOREIGN KEY (
--- `visit_wip`
---)
---REFERENCES `visitor` (
--- `visit_wip`
---);
---
---ALTER TABLE `User` ADD CONSTRAINT `FK_visitor_TO_User_2` FOREIGN KEY (
--- `visit_date`
---)
---REFERENCES `visitor` (
--- `visit_date`
---);
---
---ALTER TABLE `Board` ADD CONSTRAINT `FK_User_TO_Board_1` FOREIGN KEY (
--- `user_id`
---)
---REFERENCES `User` (
--- `user_id`
---);
---
---ALTER TABLE `Board` ADD CONSTRAINT `FK_User_TO_Board_2` FOREIGN KEY (
--- `visit_wip`
---)
---REFERENCES `User` (
--- `visit_wip`
---);
---
---ALTER TABLE `Board` ADD CONSTRAINT `FK_User_TO_Board_3` FOREIGN KEY (
--- `visit_date`
---)
---REFERENCES `User` (
--- `visit_date`
---);
---
---ALTER TABLE `Board_comment` ADD CONSTRAINT `FK_Board_TO_Board_comment_1` FOREIGN KEY (
--- `board_seq`
---)
---REFERENCES `Board` (
--- `board_seq`
---);
---
---ALTER TABLE `Board_comment` ADD CONSTRAINT `FK_User_TO_Board_comment_1` FOREIGN KEY (
--- `user_id`
---)
---REFERENCES `User` (
--- `user_id`
---);
---
---ALTER TABLE `Argument_view` ADD CONSTRAINT `FK_User_TO_Argument_view_1` FOREIGN KEY (
--- `user_id`
---)
---REFERENCES `User` (
--- `user_id`
---);
---
---ALTER TABLE `Argument_view` ADD CONSTRAINT `FK_Argument_TO_Argument_view_1` FOREIGN KEY (
--- `argu_seq`
---)
---REFERENCES `Argument` (
--- `argu_seq`
---);
---
---ALTER TABLE `codewiki_comment` ADD CONSTRAINT `FK_codewiki_TO_codewiki_comment_1` FOREIGN KEY (
--- `code_seq`
---)
---REFERENCES `codewiki` (
--- `code_seq`
---);
---
---ALTER TABLE `codewiki_comment` ADD CONSTRAINT `FK_User_TO_codewiki_comment_1` FOREIGN KEY (
--- `user_id`
---)
---REFERENCES `User` (
--- `user_id`
---);
---
---ALTER TABLE `codewiki_list` ADD CONSTRAINT `FK_codewiki_TO_codewiki_list_1` FOREIGN KEY (
--- `code_seq`
---)
---REFERENCES `codewiki` (
--- `code_seq`
---);
+--관리자 id 생성
+insert into User values (
+'admin',  
+'123456', 
+'admin@codewiki.co.kr', 
+'administrator', 
+'2020-03-01', 
+'5', 
+'',
+'000.000.000.000', 
+'2020-03-01'
+);
+
